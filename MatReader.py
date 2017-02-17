8ce4bc4469b2b6a17348b3ff42aa011c1fb07889
Ilya <iklubov@gmail.com>
2017-02-18 00:41:37 +0300

    example 2 start data

--- MatReader.py
+++ MatReader.py
@@ -1,12 +1,37 @@
+from builtins import dict as dictdict
+
 import numpy
+import time
 from scipy import io
 def addBiases(matrix):
     return numpy.c_[matrix, numpy.ones((matrix.shape[0], 1))]
 
-def getData(datasetNum):
+def getData1(datasetNum):
     dataDict = io.loadmat('res/dataset%s.mat' % str(datasetNum))
     negativeMatrix = addBiases(dataDict['neg_examples_nobias'])
     positiveMatrix = addBiases(dataDict['pos_examples_nobias'])
     weights = numpy.array(dataDict['w_init']).reshape((1,3))[0]
     return negativeMatrix, positiveMatrix, weights
 
+def getData2():
+    dataDict = io.loadmat('res/library.mat')['data']
+    vocab = numpy.matrix(dataDict['trainData'][0][0][0])
+    train = getMatrix(dataDict, 'trainData')
+    train_x = train[3]
+   # validData = numpy.matrix(dataDict['validData'][0][0])
+    testData = dataDict['testData'][0][0]
+    start_time = time.time()
+    print('T',train_x[0][0][0])
+    #tranReverse = [(vocab[i][j] for j in range(vocab.shape[0])) for i in range(vocab.shape[1])]
+   # print('TRANSPOSE', vocab)
+    elapsed_time = time.time() - start_time
+   # print('EX2 GET DATA', elapsed_time)
+    #return negativeMatrix, positiveMatrix, weights
+
+def getMatrix(dataDict,dataField):
+    print('getMatrix', numpy.matrix(dataDict[dataField][0][0]))#[0][0].shape)
+    return numpy.matrix(dataDict[dataField][0][0])
+
+def printFields(*data):
+    for d in data:
+        print('FIELDS', isinstance(data, (dict)))#, [k for k in d.keys()] if isinstance(data, dict) else [k.size for k in data])
--- NeuronExamples.py
+++ NeuronExamples.py
@@ -15,35 +15,55 @@ def ketchup():
         neuron.weights = newWeights
 
 #exercise one - we are starting
-def linearModel():
-    negMat, posMat, weights = MatReader.getData(1)
-    neuron = Neuron()
-    neuron.learningRate = 0.3
-    neuron.weights = weights
-    neuron.type = NeuronTypes.LINEAR
-    def updateWeighs():
-        numMistakes = 0
-        deltaw = numpy.zeros(negMat.shape[1])
+class LinearModel():
+    def __init__(self):
+        negMat, posMat, weights = MatReader.getData1(1)
+        self.neuron = Neuron(learningRate=0.2, weights=weights, type=NeuronTypes.LINEAR)
+        self.numMistakes, self.deltaWeights = 1, 1
+        while self.numMistakes > 0:
+            self.countSystem(negMat, posMat, self.neuron)
+            self.neuron.learningRate += 0.1
+            self.deltaWeights = 1
+            print('Mistakes', self.numMistakes)
 
+    def updateWeighs(self, negMat, posMat, neuron):
+        deltaw = numpy.zeros(negMat.shape[1])
+        mistakes = 0
         for i in range(negMat.shape[1]+1):
             neuron.inputs = negMat[i]
             if neuron.output > 0:
-                numMistakes += 1
+                mistakes += 1
 
                 deltaw += neuron.dw
 
             neuron.inputs = posMat[i]
             if neuron.output < 0:
-                numMistakes += 1
+                mistakes += 1
                 deltaw += neuron.dw
-        print('new weights', neuron.weights + deltaw, 'num mistakes', numMistakes)
+        print('new weights', neuron.weights + deltaw, 'num mistakes', self.numMistakes, 'learningRate', self.neuron.learningRate)
         neuron.weights = neuron.weights + deltaw
-        return numMistakes
-        pass
-    numMistakes = updateWeighs()
-    while numMistakes > 0:
-        numMistakes = updateWeighs()
+        return mistakes
+
+    def countSystem(self, *args):
+        while self.deltaWeights > 0:
+            lastWeights = self.neuron.weights.copy()
+            self.numMistakes = self.updateWeighs(*args)
+            self.deltaWeights = numpy.linalg.norm(lastWeights - self.neuron.weights)
+            print('delta weights', numpy.linalg.norm(lastWeights - self.neuron.weights))
+
+
+
+import matplotlib.pyplot as plt
+def plot(data):
+    plt.plot(data, 'bo')
+    plt.ylabel('some numbers')
+    plt.show()
 
 #ketchup()
-linearModel()
+#linearModel = LinearModel()
+
+def library():
+    MatReader.getData2()
+
+library()
 
