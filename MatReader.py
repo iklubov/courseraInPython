c0ed38f71665bf6fbdf4f7d7434a0cf059b9463c
Ilya <iklubov@gmail.com>
2017-02-19 23:47:51 +0300

    example 2 start data works, minibatches creation

--- MatReader.py
+++ MatReader.py
@@ -2,6 +2,8 @@ from builtins import dict as dictdict
 
 import numpy
 import time
+
+from numpy.core.multiarray import ndarray
 from scipy import io
 def addBiases(matrix):
     return numpy.c_[matrix, numpy.ones((matrix.shape[0], 1))]
@@ -15,22 +17,46 @@ def getData1(datasetNum):
 
 def getData2():
     dataDict = io.loadmat('res/library.mat')['data']
-    vocab = numpy.matrix(dataDict['trainData'][0][0][0])
-    train = getMatrix(dataDict, 'trainData')
-    train_x = train[3]
-   # validData = numpy.matrix(dataDict['validData'][0][0])
-    testData = dataDict['testData'][0][0]
     start_time = time.time()
-    print('T',train_x[0][0][0])
-    #tranReverse = [(vocab[i][j] for j in range(vocab.shape[0])) for i in range(vocab.shape[1])]
-   # print('TRANSPOSE', vocab)
+    vocab = numpy.matrix(dataDict['vocab'][0][0][0])
+    #print('VOCAB DATA INPUT', vocab)
+
+    valid = getMatrix(dataDict, 'validData')
+    valid_t = valid[0:len(valid)-1]
+    valid_x = valid[len(valid)-1]
+    #print('VALID DATA INPUT', valid_t, 'OUTPUT', valid_x)
+
+    test = getMatrix(dataDict, 'testData')
+    test_t = test[0:len(test) - 1]
+    test_x = test[len(test) - 1]
+    #print('TEST DATA INPUT', test_t, 'OUTPUT', test_x)
+
+    train = getMatrix(dataDict, 'trainData')
+    train_t = getBatchedData(train[0:len(train) - 1], 100)
+    train_x = getBatchedData(train[len(train) - 1], 100)
+    #print('TRAIN DATA INPUT', train_t.shape, 'OUTPUT', train_x.shape)
+
+
     elapsed_time = time.time() - start_time
-   # print('EX2 GET DATA', elapsed_time)
-    #return negativeMatrix, positiveMatrix, weights
+    print('ELAPSED', elapsed_time)
 
 def getMatrix(dataDict,dataField):
-    print('getMatrix', numpy.matrix(dataDict[dataField][0][0]))#[0][0].shape)
-    return numpy.matrix(dataDict[dataField][0][0])
+    rawdata = dataDict[dataField][0][0]
+
+    #print('getMatrix', rawdata[0], len(rawdata), [r for r in rawdata])
+    return rawdata
+
+def getBatchedData(data, batchSize=1):
+    dim = 1 if len(data.shape) == 1 else data.shape[0],  data.shape[0] if len(data.shape) == 1 else data.shape[1]
+    result = numpy.ndarray((int(dim[1]/batchSize),), ndarray)
+    for n in range(0, dim[1], batchSize):
+        if dim[0] > 1:
+            minibatch = data[:, n:(n+batchSize)]
+        else:
+            minibatch = data[n:(n + batchSize)]
+        if len(minibatch) == batchSize:
+            result[int(n/batchSize)] = minibatch
+    return result
 
 def printFields(*data):
     for d in data:
